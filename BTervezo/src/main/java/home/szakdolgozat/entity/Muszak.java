584bda0bccc9817add4d60055495bbdf09b509f8
MaZsolti <mazsolti@gmail.com>
2020-04-22 19:54:31 +0200

    2020.04.22

--- BTervezo/src/main/java/home/szakdolgozat/entity/Muszak.java
+++ BTervezo/src/main/java/home/szakdolgozat/entity/Muszak.java
@@ -22,36 +22,60 @@ public class Muszak {
 	private String muszak;
 	private int tol;
 	private int ig;
-	
+
 	@ManyToMany(cascade = CascadeType.ALL, mappedBy = "muszakok")
 	private List<MTerulet> mteruletek = new ArrayList<MTerulet>();
 	@ManyToMany(cascade = CascadeType.ALL, mappedBy = "muszakok")
 	private List<Dolgozo> dolgozok = new ArrayList<Dolgozo>();
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getMuszak() {
 		return muszak;
 	}
+
 	public void setMuszak(String muszak) {
 		this.muszak = muszak;
 	}
+
 	public int getTol() {
 		return tol;
 	}
+
 	public void setTol(int tol) {
 		this.tol = tol;
 	}
+
 	public int getIg() {
 		return ig;
 	}
+
 	public void setIg(int ig) {
 		this.ig = ig;
 	}
+
+	public List<MTerulet> getMteruletek() {
+		return mteruletek;
+	}
+
+	public void setMteruletek(List<MTerulet> mteruletek) {
+		this.mteruletek = mteruletek;
+	}
+
+	public List<Dolgozo> getDolgozok() {
+		return dolgozok;
+	}
+
+	public void setDolgozok(List<Dolgozo> dolgozok) {
+		this.dolgozok = dolgozok;
+	}
+
 	@Override
 	public String toString() {
 		final int maxLen = 10;
@@ -60,13 +84,5 @@ public class Muszak {
 				+ ", dolgozok=" + (dolgozok != null ? dolgozok.subList(0, Math.min(dolgozok.size(), maxLen)) : null)
 				+ "]";
 	}
-	
-	
-
-	
-
-	
-
-	
 
 }
